9df7c51b3dbf99602d028b0dae250aef91aec110
Michael Matuzak <mmatuzak@gmail.com>
2013-11-27 20:34:34 -0800

    pass in a config object instead of container to Queue

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 module.exports = Queue;
 
-function Queue(container) {
-    this.node = document.createElement('ul');
-    this.node.className = container;
-    document.body.appendChild(this.node);
+function Queue(options) {
+    if (!options) {
+        options = {};
+    }
+
+    this.container = options.container || 'queue';
+    this.tag = options.tag || 'ul';
+    this.parent = options.parent || document.body;
+
+    if (options.childTag) {
+        this.childTag = options.childTag;
+    } else {
+        this.childTag = this.tag === 'ul' ? 'li': 'span';
+    }
+
+    this.node = document.createElement(this.tag);
+    this.node.className = this.container;
+    this.parent.appendChild(this.node);
 }
 
 Queue.prototype.push = function (item) {
-    var child = document.createElement('li');
+    var child = document.createElement(this.childTag);
     child.innerHTML = item;
     child.className = 'item';
     this.node.appendChild(child);
